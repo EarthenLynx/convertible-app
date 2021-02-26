aa9221b804b12a448d6e1f759282c5da085ea0b9
Tobias Quante <t.quante@outlook.com>
2021-02-26 18:57:18 +0100

    Added pdf conversion path

---
+++ bin/deploy.bash
@@ -0,0 +1,2 @@
+#/!bin/bash
+scp -r ./convertible pi@192.168.2.159:/home/pi/static/apps/
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,4 @@
 module.exports = {
-  publicPath: '/apps/convertible/dist/',
-  outputDir: './dist/',
-  assetsPublicPath: '/apps/convertible/dist/',
-  assetsSubDirectory: 'static',
+  publicPath: '/apps/convertible/',
+  outputDir: './convertible/',
 };
